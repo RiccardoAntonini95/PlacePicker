bc68d41c74fffc027ffa20083a31e85267914c41
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-08-10 17:30:45 +0200

    fetch data with .then or async/await

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,33 @@
-import Places from './Places.jsx';
+import Places from "./Places.jsx";
+import { useState, useEffect } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  //FETCH con async/await necessito di un'altra funzione interna perchè non posso mettere async direttamente a useEffect
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("http://localhost:3000/places");
+      const data = await res.json();
+      setAvailablePlaces(data.places);
+    }
+
+    fetchData();
+  }, []);
+  /* FETCH CON .THEN
+   useEffect(() => {
+    fetch('http://localhost:3000/places')
+    .then(res => res.json())
+    .then(data => {
+      setAvailablePlaces(data.places)
+    }
+    )
+  }, []) */
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
