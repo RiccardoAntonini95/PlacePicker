0ff04b06feac91b3ce2b3a217ea69606510f810d
RiccardoAntonini95 <riccardo.antonini95@gmail.com>
2024-08-10 18:03:20 +0200

    handling errors with try/catch

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,15 +1,28 @@
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 import { useState, useEffect } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState()
 
   //FETCH con async/await necessito di un'altra funzione interna perchè non posso mettere async direttamente a useEffect
   useEffect(() => {
     async function fetchData() {
-      const res = await fetch("http://localhost:3000/places");
-      const data = await res.json();
-      setAvailablePlaces(data.places);
+      setIsFetching(true);
+      try {
+        const res = await fetch("http://localhost:3000/places");
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error("Failed to fetch places");
+        }
+        setAvailablePlaces(data.places);
+      } catch (error) {
+        setError(error)
+      } finally{
+        setIsFetching(false);
+      }
     }
 
     fetchData();
@@ -24,11 +37,17 @@ export default function AvailablePlaces({ onSelectPlace }) {
     )
   }, []) */
 
+  if(error){
+    return <Error title="An error occured" message={error.message} />
+  }
+
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
       fallbackText="No places available."
+      loadingText="Fetching data..."
+      isLoading={isFetching}
       onSelectPlace={onSelectPlace}
     />
   );
